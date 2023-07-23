1e641dbf5762d30d5f8d95b230f76b8743bb0158
Nema Ajybi <127358180+nemaajybi@users.noreply.github.com>
2023-07-23 21:28:34 +0200

    add function2

---
+++ function2.py
@@ -0,0 +1,11 @@
+x='m'
+f="m"
+z='''m'''
+print(x,f,z)
+print(x+x)
+print(x*5)
+fname = 'Mohammed'
+lname = 'Ali'
+print("hello,(). you are()").format(fname,lname)
+print(f"hello my name is (fname) (lname)")
+
