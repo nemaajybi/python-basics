bac5d5f9cdc9393fe67829d31caef4cc6de9a33b
Nema Ajybi <127358180+nemaajybi@users.noreply.github.com>
2023-07-23 21:42:18 +0200

    add function2

--- function2.py
+++ function2.py
@@ -1,11 +1,14 @@
 x='m'
-f="m"
+f='m'
 z='''m'''
 print(x,f,z)
 print(x+x)
 print(x*5)
-fname = 'Mohammed'
-lname = 'Ali'
-print("hello,(). you are()").format(fname,lname)
-print(f"hello my name is (fname) (lname)")
+
+
+fname = 'mohammed'
+lname = 'ali'
+
+
+print(f"hello my name is {fname} {lname}")
 
